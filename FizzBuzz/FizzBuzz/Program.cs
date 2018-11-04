ec653d26e1ac011a6d06014f75abf1440babe602
WhiskyTest <matt_bourne@outlook.com>
2018-11-04 15:25:03 +0000

    Finishing exercise using TDD
    TDD using MSTest, dotnet watch.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzService
+    {
+        public string Print(int n)
+        {
+            if ((n % 3 == 0) && (n % 5 == 0))
+                return "FizzBuzz";
+            else if (n % 3 == 0)
+                return "Fizz";
+            else if (n % 5 == 0)
+                return "Buzz";
+            else
+                return n.ToString();
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Print introduction */
+            Console.WriteLine("~= FizzBuzz =~");
+            Console.WriteLine("This program prints out numbers from 1 to 100.");
+            Console.WriteLine("However, for numbers divisible by 3, print \"Fizz\"");
+            Console.WriteLine("For numbers divisible by 5, print \"Buzz\"");
+            Console.WriteLine("And for numbers divisible by both 3 and 5, print \"FizzBuzz\"");
+            Console.WriteLine("\r\nPress Enter to begin:_");
+
+            /* Wait for user to press Enter */
+            Console.ReadKey();
+
+            /* Create a new instance of FizzBuzzServer */
+            FizzBuzzService _fizzbuzz = new FizzBuzzService();
+
+            /* Begin loop, checking x for it's "divisibility" */
+            for (int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine("{0}", _fizzbuzz.Print(x));
+
+            }
+
+            /* Wait for user to press Enter */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FizzBuzz/UnitTestFizzBuzz/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private FizzBuzz.FizzBuzzService _fizzBuzz;
+
+
+        public UnitTest1()
+        {
+            _fizzBuzz = new FizzBuzz.FizzBuzzService();
+        }
+
+        [TestMethod]
+        public void PrintNumber()
+        {
+            Assert.AreEqual("2", _fizzBuzz.Print(2));
+        }
+
+        [TestMethod]
+        public void PrintFizz()
+        {
+            Assert.AreEqual("Fizz", _fizzBuzz.Print(3));
+        }
+
+        [TestMethod]
+        public void PrintBuzz()
+        {
+            Assert.AreEqual("Buzz", _fizzBuzz.Print(5));
+        }
+
+        [TestMethod]
+        public void PrintFizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Print(15));
+        }
+    }
+}
